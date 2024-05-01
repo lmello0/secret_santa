af282e1af499b40506c21ee99ca4598e3f238fa9
lmello <melloluc02@gmail.com>
2024-05-01 19:21:47 -0300

    :sparkles: feat: Changing algorithms to generate code and adminCode

--- src/main/java/br/com/lmello/secret_santa/model/Draw.java
+++ src/main/java/br/com/lmello/secret_santa/model/Draw.java
@@ -1,6 +1,7 @@
 package br.com.lmello.secret_santa.model;
 
 import br.com.lmello.secret_santa.dto.DrawDTO;
+import br.com.lmello.secret_santa.util.Dictionary;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,8 +9,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Table(name = "draws")
 @Entity(name = "Draw")
@@ -51,5 +50,47 @@ public class Draw {
 
     public void startDraw() {
         this.started = true;
+    private String generateCode() {
+        final int SECTIONS = 3;
+        final int SECTION_LENGTH = 3;
+
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < SECTIONS; i++) {
+            for (int j = 0; j < SECTION_LENGTH; j++) {
+                int randomIndex = (int) (Math.random() * alphabet.length());
+                char randomChar = alphabet.charAt(randomIndex);
+
+                sb.append(randomChar);
+            }
+
+            if (i < 2) {
+                sb.append("-");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private String generateAdminCode() {
+        final int SECTIONS = 3;
+        final int WORDS_SIZE = Dictionary.WORDS.size();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < SECTIONS; i++) {
+            int randomIndex = (int) (Math.random() * WORDS_SIZE);
+
+            String randomWord = Dictionary.WORDS.get(randomIndex);
+
+            sb.append(randomWord);
+
+            if (i < 2) {
+                sb.append("-");
+            }
+        }
+
+        return sb.toString();
     }
 }
