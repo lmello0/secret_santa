d3133aac2be5b4cf868a9c4be2c4faa6241e7922
lmello <melloluc02@gmail.com>
2024-05-01 19:43:10 -0300

    :sparkles: feat: Adding more fields to DrawDTO, to be used both at data input and output

--- src/main/java/br/com/lmello/secret_santa/dto/DrawDTO.java
+++ src/main/java/br/com/lmello/secret_santa/dto/DrawDTO.java
@@ -1,22 +1,29 @@
 package br.com.lmello.secret_santa.dto;
 
 import br.com.lmello.secret_santa.model.Draw;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DrawDTO(
+        String code,
+        String adminCode,
+        boolean started,
         @JsonProperty
         @NotNull
         int budget,
-
         @JsonProperty
         @NotNull
         List<ParticipantDTO> participants
 ) {
     public DrawDTO(Draw draw) {
         this(
+                draw.getCode(),
+                draw.getAdminCode(),
+                draw.isStarted(),
                 draw.getBudget(),
                 draw.getParticipants()
                         .stream()
