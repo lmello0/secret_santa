fd7f8023e82d29d7a83da593dda1ee1cea5646d9
lmello <melloluc02@gmail.com>
2024-05-02 17:14:59 -0300

    :test_tube: test: Adding test to check if drawController was created successfully

--- src/test/java/br/com/lmello/secret_santa/SecretSantaApplicationTests.java
+++ src/test/java/br/com/lmello/secret_santa/SecretSantaApplicationTests.java
@@ -1,13 +1,30 @@
 package br.com.lmello.secret_santa;
 
+import br.com.lmello.secret_santa.controller.DrawController;
+import br.com.lmello.secret_santa.service.DrawService;
+import br.com.lmello.secret_santa.service.EmailService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class SecretSantaApplicationTests {
+public class SecretSantaApplicationTests {
+    @Autowired
+    private DrawController drawController;
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    private DrawService drawService;
 
+    @MockBean
+    private EmailService emailService;
+
+    @Test
+    @DisplayName("DrawController should not be null")
+    public void drawControllerShouldNotBeNull() {
+        assertThat(drawController).isNotNull();
+    }
 }
