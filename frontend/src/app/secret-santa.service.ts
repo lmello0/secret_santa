e16f3cbefdd15fe46e01b0b9e1aed0d335cfa097
lmello <melloluc02@gmail.com>
2024-05-14 21:48:25 -0300

    :recycle: refactor: Add backendUrl in environment config

--- frontend/src/app/secret-santa.service.ts
+++ frontend/src/app/secret-santa.service.ts
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class SecretSantaService {
-  private readonly API = '/draw';
+  private readonly API = environment.backendUrl + '/draw';
   private headers = new HttpHeaders({ 'X-API-KEY': environment.apiKey });
 
   constructor(private http: HttpClient) { }
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiKey: 'super-dev-key'
+  apiKey: 'super-dev-key',
+  backendUrl: 'http://localhost:8080'
 }
